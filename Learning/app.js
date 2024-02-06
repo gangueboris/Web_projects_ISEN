c213b2a476ac22dd30f73cd1e00723eae56a8842
gangueboris <gangueboris@gmail.com>
2024-02-06 23:51:48 +0100

    Update

--- Learning/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menu = document.querySelector('#mobile-menu');
-    const menuLinks = document.querySelector('.navbar_menu');
-
-    menu.addEventListener('click', function(){    
-        menu.classList.toggle('is-active');
-        menuLinks.classList.toggle('active');
-    });
-});
